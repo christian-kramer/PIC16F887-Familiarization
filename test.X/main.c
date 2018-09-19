8c6ea780c6a79c4ef80f82272f5cb5b3ddad22f0
christian <christian.kramer@mail.com>
2018-09-19 02:37:31 -0500

    Prototype of Light Sensor algorithm for PIC10F220

--- test.X/main.c
+++ test.X/main.c
@@ -5,27 +5,35 @@
  * Created on September 3, 2018, 5:28 AM
  */
 
-//#define _XTAL_FREQ 8000000
+// #define _XTAL_FREQ 8000000
 #include <stdio.h>
 #include <stdlib.h>
 #include <xc.h>
-#pragma config FOSC=INTRC_NOCLKOUT, WDTE=OFF, MCLRE=ON, LVP=OFF     
+#pragma config FOSC=INTRC_NOCLKOUT, WDTE=OFF, MCLRE=OFF, LVP=OFF     
 
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+
+unsigned int getLightValue()
+{
+    ADCON0bits.GO = 1;
+    return ~ADRESH;
+}
+
+
+void main() {
 
     //ANSEL   = 0;
-    ANSELH  = 0x2F;     // Analog/Digital, --101111
-    TRISA   = 0x01;     // I/O
-    TRISB   = 0x01;     // I/O
-    TRISD   = 0x00;     // I/O
-    ADCON0  = 0x01;     // clock select, channel select, go, on
+    //WPUB    = 0x00;
+    ////ANSELH  = 0x3D;     // Analog/Digital, --101111
+    ////TRISB   = 0xF7;     // I/O
+    ////TRISD   = 0x00;     // I/O
+    ////ADCON0  = 0x25;     // clock select, channel select, go, on
     //ADCON1  = 0x00;   // right or left justified, negative and positive reference voltages
     
-    
+    /*
     TMR0    = 0;        // instantiate timer at 0
     T0CS    = 0;        // select either internal or external source
     T0SE    = 0;        // XOR bit for T0CKI pin
@@ -33,40 +41,33 @@ int main(int argc, char** argv) {
     PS0     = 1;        // selection for prescaler
     PS1     = 1;        // selection for prescaler
     PS2     = 1;        // selection for prescaler
+    */
     
-    int count;
+    TRISA   = 0x02; //00000100
+    TRISD	= 0x00;
+    ADCON0  = 0x05;
+    
+    unsigned int startValue = getLightValue();
+    unsigned int count = 0;
     
     while (1) {
-        if (T0IF)
+        unsigned int currentValue = getLightValue();
+        if (currentValue > startValue + 5 && count < 100)
         {
             count++;
-            T0IF = 0;
         }
-        if (count == 256)
+        if (currentValue < startValue - 5 && count > 0)
         {
-            count = 0;
+            count--;
         }
-        
-        if (~PORTB&1)
+        if (count == 100)
         {
-            ADCON0bits.GO = 1;
-            
-            PORTD = ADRESH;
+            PORTD = 0xFF;
         }
-        else
+        if (count == 0)
         {
-            if (count&0x1F)
-            {
-                PORTD   = 0x00;
-            }
-            else
-            {
-                PORTD   = 0x01;
-            }
+            PORTD = 0x01;
         }
     }
-    
-    
-    return (EXIT_SUCCESS);
 }
 
